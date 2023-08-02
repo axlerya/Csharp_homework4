33d672856a5c25ce2c4b7feaef49ab567e9c53ef
axlerya <white_cat_02@mail.ru>
2023-08-02 17:16:27 +0300

    feat: add task 1
    add a solution to the first task

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите два числа");
+int numberInputOne = Convert.ToInt32(Console.ReadLine());
+int numberInputTwo = Convert.ToInt32(Console.ReadLine());
+int x = (int)Math.Pow(numberInputOne, numberInputTwo);
+Console.WriteLine($"Число {numberInputOne} в степени {numberInputTwo} = {x}");
