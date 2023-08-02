52622a668b6c401cb40f7b578922f167a30eb7aa
axlerya <white_cat_02@mail.ru>
2023-08-02 17:35:37 +0300

    feat: add task 2
    add a solution to the second task

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] GetIntArray(int num) //перевод числа в массив 
+{
+    List<int> listOfInts = new();
+    while(num > 0)
+    {
+        listOfInts.Add(num % 10);
+        num /= 10;
+    }
+    listOfInts.Reverse();
+    int[] arrayNumbersOne = listOfInts.ToArray();
+    return arrayNumbersOne;
+}
+
+Console.WriteLine("Введите число");
+int numberInput = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = GetIntArray(numberInput);
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    sum += arr[i];
+}
+
+Console.WriteLine(sum);
