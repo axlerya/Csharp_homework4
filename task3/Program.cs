d932cebfeb6ac569ca0ba0d1b001236c20456047
axlerya <white_cat_02@mail.ru>
2023-08-02 17:58:49 +0300

    feat: add task 3
    add a solution to the third task

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] arr = Console.ReadLine()
+                .Split(',', ' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+Console.Write("[");
+Console.Write(string.Join(", ", arr));
+Console.Write("]");
